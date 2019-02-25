7093fd908a94e2bcf2fb36b69d8742a78c8a3e62
Tim <Tim@Tims-MacBook-Pro.local>
2019-02-26 00:29:12 +0700

    Netlify Commit

---
+++ dist/js/contact.js
@@ -0,0 +1,18 @@
+// listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e) {
+  e.preventDefault();
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function() {
+    document.querySelector('.alert').style.display = 'none';
+  }, 3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
